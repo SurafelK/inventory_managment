d496dfdf9357f86df981534f75f744ce18edfca6
Surafel Kassahun <babishufer@gmail.com>
2024-01-02 09:20:34 +0300

    Profile With Protected Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,10 +6,12 @@ const bcrypt = require('bcryptjs')
 // Generate Token
 const generateToken = (id) =>
 {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
-}
+    return jwt.sign({ id },process.env.JWT_SECRET, {expiresIn:
+    "1d" });
+  
+};
 
-
+ 
 // Register User
 
 const registerUser = asyncHandler( async(req,res) =>
@@ -44,14 +46,13 @@ const registerUser = asyncHandler( async(req,res) =>
 
     // Create new User
     const user = await User.create({
-        
         name,
         email,
         password
     })
 
      // Generate Token
-     const token = generateToken(User._id);
+     const token = generateToken(user._id);
 
     //  Send HTTP-Only Cookie
     res.cookie("token", token, {
@@ -60,7 +61,7 @@ const registerUser = asyncHandler( async(req,res) =>
         expires: new Date(Date.now() + 1000 * 86400),    // 1day
         sameSite: 'none',
         secure: true 
-    })
+    });
 
     if (user) 
     {
@@ -68,6 +69,7 @@ const registerUser = asyncHandler( async(req,res) =>
 
         res.status(201).json(
             {
+                _id,
                 name,
                 email,
                 photo,
@@ -90,32 +92,27 @@ const loginUser = asyncHandler( async  (req,res) =>
    const {email,password} = req.body;
 
 //    Validate the request
-
-
     if(!email || !password)
     {
         res.status (400)
 
         throw new Error("Please add email and Password")
     }
-
     // Check user exists
-
     const user = await User.findOne({email})
-
+   
     if(!user)
     {
         res.status(400)
 
-        throw new Error('User Not Found, Please Register')
+        throw new Error('User Not Found, Please Signup')
     }
 
     // User exist, Check if Password is Correct
-
     const passwordIsCorrect = await bcrypt.compare(password, user.password);
    
     // Generate Token
-    const token = generateToken(User._id);
+    const token = generateToken(user._id);
 
     //  Send HTTP-Only Cookie
     res.cookie("token", token, {
@@ -142,10 +139,6 @@ const loginUser = asyncHandler( async  (req,res) =>
         });
 
 
-
-
-
-
     } else{
         res.status(400)
 
@@ -155,8 +148,77 @@ const loginUser = asyncHandler( async  (req,res) =>
 
 }) 
 
+const logoutUser = asyncHandler( async (req,res) =>
+{
+    res.cookie("token", "", {
+        path:"/",
+        httpOnly: true,
+        expires: new Date(Date(0)),    // Now
+        sameSite: 'none',
+        secure: true 
+    });
+
+    return res.status(200).json({
+        message: "Successfully Logged out"
+    })
+
+} )
+
+// Get User Profile
+const getUser = asyncHandler( async(req,res)=>
+{
+    const user = await User.findById(req.user._id)
+    
+    if (user)
+    {
+        const { _id, name, email, photo, phone, bio } = user
+
+        res.status(200).json(
+            {
+                _id,
+                name,
+                email,
+                photo,
+                phone,
+                bio
+            }
+        )
+    }else{
+        res.status(400)
+
+        throw new Error('User Not Found')
+    }
+} )
+
+
+// Get Login Status
+
+const loginStatus = asyncHandler(async(req,res)=>
+{
+
+    const token = req.cookies.token;
+
+    if(!token)
+    {
+        res.json(false)
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if(verified)
+    {
+        res.json(true)
+    }
+
+    res.send("Login Status")
+})
+
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser,
+    getUser,
+    loginStatus
 };
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const {registerUser,loginUser} = require('../controllers/userController');
+const {registerUser,loginUser, logoutUser, getUser, loginStatus} = require('../controllers/userController');
+const protect = require('../middleware/authMiddleware');
 const router = express.Router();
 
 
-router.post('/register', registerUser)
-router.post('/login', loginUser)
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/logout', logoutUser);
+router.get('/getuser', protect, getUser)
+router.get('/loggedin', loginStatus)
+
 
 module.exports = router
